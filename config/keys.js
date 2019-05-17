076ff9b31755f4496e95d64e46e44b8f592005a8
elcid00 <elcid00@aol.com>
2019-05-17 15:41:48 -0400

    connected to db

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://admin:adminpswd123@userauth-tlo1g.mongodb.net/test?retryWrites=true'
+	mongoURI: 'mongodb+srv://admin:adminpswd123@userauth-tlo1g.mongodb.net/test?retryWrites=true',
+	secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const users = require('./routes/api/users');
 
 // Middleware
 app.use(
@@ -19,7 +21,17 @@ mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log('Successfully connected to DB'))
 	.catch((err) => console.log(err));
-const port = process.env.PORT || 5000;
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
+
 // process.env.port is Heroku's port
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port} !`));
