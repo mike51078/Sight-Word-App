a89a01fa980bec4618d9b39f49c32ba4bd2725ba
elcid00 <elcid00@aol.com>
2019-05-18 23:56:24 -0400

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { Provider } from 'react-redux';
+import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
+import privateRoute from './components/private-route/privateRoute';
+import dashboard from './components/dashboard/dashboard';
+import './App.css';
+
+if (localStorage.jwtToken) {
+	const token = localStorage.jwtToken;
+	setAuthToken(token);
+	const decoded = jwt_decode(token);
+	store.dispatch(setCurrentUser(decoded));
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		store.dispatch(logoutUser());
+		window.location.href = './login';
+	}
+}
 
 class App extends Component {
 	render() {
@@ -18,6 +35,9 @@ class App extends Component {
 						<Route exact path="/" component={Landing} />
 						<Route exact path="/register" component={Register} />
 						<Route exact path="/login" component={Login} />
+						<Switch>
+							<privateRoute exact path="/dashboard" component={dashboard} />
+						</Switch>
 					</div>
 				</Router>
 			</Provider>
