7bcf9ccf838aeaaa2c8935f73c004de7abb16982
elcid00 <elcid00@aol.com>
2019-05-19 17:24:07 -0400

    login fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { setCurrentUser, logoutUser } from './actions/authActions';
-import { Provider } from 'react-redux';
-import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import PrivateRoute from './components/private-route/privateRoute';
-import dashboard from './components/dashboard/dashboard';
-import './App.css';
+import PrivateRoute from './components/private-route/PrivateRoute';
+import Dashboard from './components/dashboard/Dashboard';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { Provider } from 'react-redux';
 
+// Check for token to keep user logged in
 if (localStorage.jwtToken) {
+	// Set auth token header auth
 	const token = localStorage.jwtToken;
 	setAuthToken(token);
+	// Decode token and get user info and exp
 	const decoded = jwt_decode(token);
+	// Set user and isAuthenticated
 	store.dispatch(setCurrentUser(decoded));
-	const currentTime = Date.now() / 1000;
+	// Check for expired token
+	const currentTime = Date.now() / 1000; // to get in milliseconds
+
 	if (decoded.exp < currentTime) {
+		// Logout user
 		store.dispatch(logoutUser());
+		// Redirect to login
 		window.location.href = './login';
 	}
 }
@@ -36,7 +43,7 @@ class App extends Component {
 						<Route exact path="/register" component={Register} />
 						<Route exact path="/login" component={Login} />
 						<Switch>
-							<PrivateRoute exact path="/dashboard" component={dashboard} />
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
 						</Switch>
 					</div>
 				</Router>
--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
-class dashboard extends Component {
+class Dashboard extends Component {
 	onLogoutClick = (e) => {
 		e.preventDefault();
 		this.props.logoutUser();
@@ -38,11 +38,11 @@ class dashboard extends Component {
 		);
 	}
 }
-dashboard.propTypes = {
+Dashboard.propTypes = {
 	logoutUser: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired
 };
 const mapStateToProps = (state) => ({
 	auth: state.auth
 });
-export default connect(mapStateToProps, { logoutUser })(dashboard);
+export default connect(mapStateToProps, { logoutUser })(Dashboard);
